c36b83f16a7a3b7766ee2018ee294181b66572ec
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-03-10 14:27:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Started my app using shotgun!!!! "
+  end
+
+  get '/name' do
+    "My name is Khaled Khalifeh"
+  end
+  get '/hometown' do
+    "My hometown is Amman, Jordan"
+  end
+  get '/favorite-song' do
+    "My favorite song is Drake Over"
+  end
+
 end
